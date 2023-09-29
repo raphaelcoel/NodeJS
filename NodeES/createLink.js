f00dd7f539dee70a2f33077f3f55ea726bc360e6
raphaelcoel <142271102+raphaelcoel@users.noreply.github.com>
2023-09-29 20:52:08 -0300

    Delete NodeES directory

--- NodeES/createLink.js
+++
@@ -1,9 +0,0 @@
-export function createLink(dir, filename) {
-    return `<a href="/${dir}/${filename}">${filename}</a><br>\n`;
-}
-
-export function voltar() {
-    return `<a href="/">Voltar</a><br><br>\n`;
-}
-
-    module.exports = createLink
